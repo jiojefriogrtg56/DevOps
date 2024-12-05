6f4e49334b32734e6f5f08dd47dfb05df0a52d33
pankratov_maksim <sdfsdfsdf.dsfdfsd@yandex.ru>
2024-12-05 14:09:02 +0300

    d

--- backend/Server.py
+++ backend/Server.py
@@ -9,14 +9,14 @@ CORS(app)
 def post_data():
        data = request.data
        print(f"Received data: {data.decode('utf-8')}")
-       with open("data.txt", "wb") as f:
+       with open("data/data.txt", "wb") as f:
            f.write(data)
        return '', 200
 
 @app.route('/data', methods=['GET'])
 def get_data():
-       if os.path.exists("data.txt"):
-           with open("data.txt", "rb") as f:
+       if os.path.exists("data/data.txt"):
+           with open("data/data.txt", "rb") as f:
                data = f.read()
            print(f"Sending data: {data.decode('utf-8')}")
            return data, 200
@@ -25,4 +25,4 @@ def get_data():
            return 'File not found', 404
 
 if __name__ == "__main__":
-       app.run(host='0.0.0.0', port=8080)  # Убедитесь, что хост установлен на '0.0.0.0'
+       app.run(host='0.0.0.0', port=8000)  # Убедитесь, что хост установлен на '0.0.0.0'
